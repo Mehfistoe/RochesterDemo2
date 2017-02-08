4940e85ffe6ac7220614b14dc0035368b0ad0500
Mehfistoe <mckroc468@outlook.com>
2017-02-07 21:40:54 -0500

    kljdflkasjfl;asdj

---
+++ app/src/main/java/com/example/rochesterdemo2/adapter/ListViewAdapter.java
@@ -0,0 +1,51 @@
+package com.example.rochesterdemo2.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.rochesterdemo2.R;
+
+/**
+ * Created by mckenzie on 2/7/17.
+ */
+
+public class ListViewAdapter extends BaseAdapter {
+
+    private final LayoutInflater mInflater;
+    private Context mContext;
+
+    public ListViewAdapter(Context context) {
+        mContext = context;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+//        TextView view = new TextView(mContext);
+//        view.setText(String.valueOf(position));
+//        return view;
+        View rowView = mInflater.inflate(R.layout.list_item, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.list_view_tv);
+        textView.setText(String.valueOf(position));
+        return rowView;
+    }
+}
